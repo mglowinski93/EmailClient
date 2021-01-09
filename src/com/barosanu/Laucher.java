951be5f49a330698aecddc7504a5f8c98ddc3687
mglowinski93 <mglowinski93@gmail.com>
2021-01-09 19:18:11 +0100

    Login Windows added

---
+++ src/com/barosanu/Laucher.java
@@ -0,0 +1,25 @@
+package com.barosanu;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Laucher extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent parent = FXMLLoader.load(getClass().getResource("view/LoginWindow.fxml"));
+
+        Scene scene = new Scene(parent, 590, 260);
+        stage.setScene(scene);
+
+        stage.show();
+    }
+}
